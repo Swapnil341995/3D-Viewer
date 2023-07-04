e1b7c22312d234873386cbe2f41f0ef17cb29598
Swapnil <sbharude.works@gmail.com>
2023-07-05 02:10:21 +0530

    no changes

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,7 @@ const app = {
   partNames: [],
   moveWings: false,
   rotateSceneObj: false,
+  clock: new THREE.Clock(),
   createCube: function (length = 10,width = 10,height = 10,color = 0x0000ff) {
     const geometry = new THREE.BoxGeometry(length, width, height);
     const material = new THREE.MeshBasicMaterial({ color: color });
@@ -73,8 +74,8 @@ const app = {
     // Load a glTF and glb resource
     loader.load(
       // resource URL
-      "./src/assets/gltf/scifi-helmet/SciFiHelmet.gltf",
-      // "./src/assets/glb/BrainStem.glb",
+      // "./src/assets/gltf/scifi-helmet/SciFiHelmet.gltf",
+      "./src/assets/glb/BrainStem.glb",
       // "./assets/glb/FormalShoe.glb",
       // "./assets/gltf/toycar/ToyCar.gltf",
       // "./assets/gltf/duck/Duck.glb",
@@ -537,6 +538,7 @@ const events = {
 
 function animate() {
   requestAnimationFrame(animate);
+  const time = app.clock.getElapsedTime();//using time for animation
   if (viewer.controls) {
     viewer.controls.update();
   }
